5c7125240aadd48587777f5e14beb7225e56def7
Rex Wang <chengche@broadinstitute.org>
2018-01-04 16:03:34 -0500

    Fix the 'An authentication token must be supplied when uploading a spreadsheet' issue.

--- tests/ingest_agents.py
+++ tests/ingest_agents.py
@@ -10,12 +10,14 @@ class IngestUIAgent:
     def __init__(self, deployment):
         self.deployment = deployment
         self.ingest_broker_url = self.INGEST_UI_URL_TEMPLATE.format(self.deployment)
+        self.ingest_auth_agent = IngestAuthAgent()
+        self.auth_headers = self.ingest_auth_agent.make_auth_header()
 
 
     def upload(self, metadata_spreadsheet_path):
         url = self.ingest_broker_url + '/api_upload'
         files = {'file': open(metadata_spreadsheet_path, 'rb')}
-        response = requests.post(url, files=files, allow_redirects=False)
+        response = requests.post(url, files=files, allow_redirects=False, headers=self.auth_headers)
         if response.status_code != requests.codes.found:
             raise RuntimeError(f"POST {url} response was {response.status_code}: {response.content}")
         # Eventually this response will be a redirect that contains the submssion ID as a query param.
