5a94e2d25b717898e52c3c62d4435d96b0765b66
Rex Wang <chengche@broadinstitute.org>
2018-01-08 22:09:44 -0500

    Update the SS2_ANALYSIS_OUTPUT_FILES_REGEXES to match the outputs of the new ss2 pipelines Green Box developed.

--- tests/test_end_to_end_dcp.py
+++ tests/test_end_to_end_dcp.py
@@ -170,16 +170,37 @@ class TestSmartSeq2Run(TestEndToEndDCP):
 
     SMARTSEQ2_BUNDLE_FIXTURE_PATH = 'tests/fixtures/bundles/Smart-seq2'
     SS2_ANALYSIS_OUTPUT_FILES_REGEXES = [
-        re.compile('^Aligned\.sortedByCoord\.out\.bam$'),
-        re.compile('^Aligned\.toTranscriptome\.out\.bam$'),
-        re.compile('^.+_rna_metrics$'),
-        re.compile('^.+_alignment_metrics$'),
-        re.compile('^.+\.genes\.results$'),
-        re.compile('^.+\.isoforms\.results$'),
-        re.compile('^.+\.gene\.expected_counts$'),
-        re.compile('^.+\.gene\.counts\.txt$'),
-        re.compile('^.+\.exon\.counts\.txt$'),
-        re.compile('^.+\.transcripts\.counts\.txt$')
+        re.compile('^.+\_qc\.bam$'),  # aligned_bam
+        re.compile('^.+\_qc\.alignment\_summary\_metrics\.txt$'),  # alignment_summary_metrics
+        re.compile('^.+\_qc\.bait\_bias\_detail\_metrics\.txt$'),  # bait_bias_detail_metrics
+        re.compile('^.+\_qc\.bait\_bias\_summary\_metrics\.txt$'),  # bait_bias_summary_metrics
+        re.compile('^.+\_qc\.base\_distribution\_by\_cycle\_metrics\.txt$'),  # base_call_dist_metrics
+        re.compile('^.+\_qc\.base\_distribution\_by\_cycle\.pdf$'),  # base_call_pdf
+        re.compile('^.+\_qc\.duplicate\_metrics\.txt$'),  # dedup_metrics
+        re.compile('^.+\_qc\.error\_summary\_metrics\.txt$'),  # error_summary_metrics
+        re.compile('^.+\_qc\.gc\_bias\.detail\_metrics\.txt$'),  # gc_bias_detail_metrics
+        re.compile('^.+\_qc\.gc\_bias\.pdf$'),  # gc_bias_dist_pdf
+        re.compile('^.+\_qc\.gc\_bias\.summary\_metrics\.txt$'),  # gc_bias_summary_metrics
+        re.compile('^.+\_qc\.insert\_size\_histogram\.pdf$'),  # insert_size_hist
+        re.compile('^.+\_qc\.insert\_size\_metrics\.txt$'),  # insert_size_metrics
+        re.compile('^.+\_qc\.log$'),  # hisat2_logfile
+        re.compile('^.+\_qc\.hisat2\.met\.txt$'),  # hisat2_metfile
+        re.compile('^.+\_qc\.pre\_adapter\_detail\_metrics\.txt$'),  # pre_adapter_details_metrics
+        re.compile('^.+\_qc\.quality\_by\_cycle\_metrics\.txt$'),  # quality_by_cycle_metrics
+        re.compile('^.+\_qc\.quality\_by\_cycle\.pdf$'),  # quality_by_cycle_pdf
+        re.compile('^.+\_qc\.quality\_distribution\.pdf$'),  # quality_distribution_dist_pdf
+        re.compile('^.+\_qc\.quality\_distribution\_metrics\.txt$'),  # quality_distribution_metrics
+        re.compile('^.+\_qc\.rna\.coverage\.pdf$'),  # rna_coverage
+        re.compile('^.+\_qc\.rna\_metrics\.txt$'),  # rna_metrics
+        re.compile('^.+\_rsem\.bam$'),  # aligned_trans_bam
+        re.compile('^.+\_rsem\.log$'),  # hisat2tran_logfile
+        re.compile('^.+\_rsem\.hisat2\.met\.txt$'),  # hisat2tran_metfile
+        re.compile('^.+\_rsem\.cnt$'),  # rsem_cnt_log
+        re.compile('^.+\_rsem\.genes\.results$'),  # rsem_gene_results
+        re.compile('^.+\_rsem\.isoforms\.results$'),  # rsem_isoform_results
+        re.compile('^.+\_rsem\.model$'),  # rsem_model_log
+        re.compile('^.+\_rsem\.theta$'),  # rsem_theta_log
+        re.compile('^.+\_rsem\.time$')  # rsem_time_log
     ]
 
     def test_smartseq2_run(self):
