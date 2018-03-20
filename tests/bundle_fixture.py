84f53bee19754716f8c529c71cb69659659aa8f0
Sam Pierson <sam@sampierson.com>
2018-03-20 08:43:22 -0700

    Update SS2 bundle metadata to v5

--- tests/bundle_fixture.py
+++ tests/bundle_fixture.py
@@ -17,7 +17,7 @@ class BundleFixture:
         self.bundle_path = bundle_path
 
     def metadata_spreadsheet_path(self):
-        xlsx_files = glob.glob(f"{self.bundle_path}/*.xlsx")
+        xlsx_files = glob.glob(f"{self.bundle_path}/*_v5_*.xlsx")
         assert len(xlsx_files) == 1, f"There is more than 1 .xlsx file in {self.bundle_path}"
         return xlsx_files[0]
 
