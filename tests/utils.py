e1d99aad1c303a39d982887cd2b06703af4f3bed
Sam Pierson <sam@sampierson.com>
2018-05-21 16:53:41 -0700

    Remove fancy ticking-clock-at-start-of-line from Progress reporter.
    We never run this stuff manually, so let's optimize for CI-friendliness.
    This also greatly simplifies WaitFor - no more polling every second.

--- tests/utils.py
+++ tests/utils.py
@@ -1,31 +1,14 @@
 from datetime import datetime
-import os
 import sys
 
 
 class Progress:
-
-    ANSI_MOVE_CURSOR_RIGHT = "\x1b[{count}C"
 
     start_time = datetime.now()
     cursor_column = 0
 
     @classmethod
     def report(cls, message):
-        if 'TRAVIS' in os.environ:
-            cls._simple_output(message)
-        else:
-            if cls.cursor_column == 0:
-                cls._output_and_record_new_cursor_column(cls._elapsed_time() + " ")
-            else:
-                cls._jump_to_start_of_line_print_time_jump_back()
-            cls._output_and_record_new_cursor_column(message)
-
-    @classmethod
-    def _simple_output(cls, message):
-        """
-        Travis does weird things with output, so do dumb line-oriented output only.  No ANSI escape sequences.
-        """
         if not message == "":
             sys.stdout.write(cls._elapsed_time() + " " + message)
             if not message.endswith("\n"):
@@ -33,23 +16,9 @@ class Progress:
             sys.stdout.flush()
 
     @classmethod
-    def _jump_to_start_of_line_print_time_jump_back(cls):
-        if sys.stdout.isatty():
-            sys.stdout.write(f"\r{cls._elapsed_time()}\r" + cls.ANSI_MOVE_CURSOR_RIGHT.format(count=cls.cursor_column))
-
-    @classmethod
     def _elapsed_time(cls):
         elapsed_delta = datetime.now() - cls.start_time
         return cls._duration_h_mm_ss(elapsed_delta.seconds)
-
-    @classmethod
-    def _output_and_record_new_cursor_column(cls, message):
-        sys.stdout.write(message)
-        sys.stdout.flush()
-        if message.find("\n") == -1:
-            cls.cursor_column += len(message)
-        else:
-            cls.cursor_column = len(message.split("\n")[-1])
 
     @staticmethod
     def _duration_h_mm_ss(duration_secs):
