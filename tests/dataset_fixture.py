bcb9bfceb974b8bb0623fbc1b369b292ca3b6451
Sam Pierson <sam@sampierson.com>
2018-06-04 18:33:03 -0700

    Make spreadsheet project_core.project_shortname unique for each run

--- tests/dataset_fixture.py
+++ tests/dataset_fixture.py
@@ -20,8 +20,9 @@ class DatasetFixture:
     """
 
     def __init__(self, dataset_name):
+        self.name = dataset_name
         self.config = {}
-        self.dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures/datasets', dataset_name))
+        self.dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures/datasets', self.name))
         self._spreadsheet = None
         readme_json_path = os.path.join(self.dataset_path, 'README.json')
         if os.path.isfile(readme_json_path):
@@ -33,6 +34,13 @@ class DatasetFixture:
 
     def data_files_are_in_s3(self):
         return self.config.get('data_files_location', '').startswith('s3://')
+
+    def update_spreadsheet_project_shortname(self, new_shortname):
+        project_tab = self.spreadsheet['project']
+        if project_tab['A3'].value != "project_core.project_shortname":
+            raise RuntimeError("project_core.project_shortname is no longer in cell project!A3")
+        project_tab['A4'] = new_shortname
+        self.spreadsheet.save(filename=self.metadata_spreadsheet_path)
 
     @property
     def metadata_spreadsheet_path(self, metadata_version="5"):
