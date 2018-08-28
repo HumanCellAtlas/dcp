5f773abe8b51bd79c08106b1466be4abdfaf8550
alex-hancock <alexbhancock@gmail.com>
2018-08-28 13:59:16 -0700

    Add functionality to retry 500+ errors from Azul webservice

--- tests/azul_agent.py
+++ tests/azul_agent.py
@@ -7,11 +7,16 @@ class AzulAgent:
     AZUL_URL_TEMPLATE = 'https://service.{deployment}.explore.data.humancellatlas.org'
 
     def __init__(self, deployment):
+        from urllib3 import Retry
         self.azul_url = self.AZUL_URL_TEMPLATE.format(deployment=deployment)
+        self.https_session = requests.Session()
+        azul_retries = Retry(status_forcelist=(500, 502, 503, 504))
+        azul_adapter = requests.adapters.HTTPAdapter(max_retries=azul_retries)
+        self.https_session.mount('https://', azul_adapter)
 
     def get_specimen_by_project(self, project_shortname):
         filters = {"file": {"project": {"is": [project_shortname]}}}
         path = '/repository/specimens?filters=' + urllib.parse.quote(json.dumps(filters))
-        response = requests.get(self.azul_url + path)
+        response = self.https_session.request("GET", self.azul_url + path)
         response.raise_for_status()
         return response.json()
