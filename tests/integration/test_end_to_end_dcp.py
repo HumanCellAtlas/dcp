d1ef018fe4c7000ca9eaf0abc0106c32219bbae0
Saman Ehsan <se8ea@virginia.edu>
2019-04-22 17:23:38 -0400

    Revert testing modifications

--- tests/integration/test_end_to_end_dcp.py
+++ tests/integration/test_end_to_end_dcp.py
@@ -171,7 +171,7 @@ class TestOptimusRun(TestEndToEndDCP):
     def test_optimus_run(self):
         runner = DatasetRunner(deployment=self.deployment)
 
-        with Timeout(15 * 60) as to:  # timeout after 3.5 hours (same as Gitlab runner setting)
+        with Timeout(210 * 60) as to:  # timeout after 3.5 hours (same as Gitlab runner setting)
             try:
                 self.ingest_store_and_analyze_dataset(runner, dataset_fixture='optimus')
                 self.assertEqual(1, len(runner.primary_bundle_uuids))
