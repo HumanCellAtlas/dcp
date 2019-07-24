ad97ff1e12d95905f31e77de2a3a2726efde34df
Rodrey Goite <rodrey@ebi.ac.uk>
2019-07-24 10:18:14 +0100

    Removed comment.

--- tests/dataset_runner.py
+++ tests/dataset_runner.py
@@ -101,9 +101,6 @@ class DatasetRunner:
                 self.wait_for_analysis_workflows()
                 self.wait_for_secondary_bundles()
 
-            #if self.dataset.name == "Smart-seq2" or self.dataset.name == "optimus":
-            #    self.retrieve_zarr_output_from_matrix_service()
-            #    self.retrieve_loom_output_from_matrix_service()
             self.assert_data_browser_bundles()
 
         if self.failure_reason:
